db15462141b87464864fb4fd87b6e5eeeaf8fe40
Mike Thompson <mithompson@staywell.com>
2020-04-18 01:03:55 -0600

    Misc fixes and clean-up

---
+++ readme.md
@@ -0,0 +1,3 @@
+# thread-throttle-poc
+
+This is a proof of concept for throttling batch message processing of an external message queue.
--- src/main/java/net/mikeyt/model/Message.java
+++ src/main/java/net/mikeyt/model/Message.java
@@ -8,10 +8,12 @@ public class Message {
     private String id;
     private String body;
     private DateTime timeReceived; // Use for visibility timeout refresh
+    private long mockProcessingMillis;
 
-    public Message(String id, String body, DateTime timeReceived) {
+    public Message(String id, String body, DateTime timeReceived, long mockProcessingMillis) {
         this.id = id;
         this.body = body;
         this.timeReceived = timeReceived;
+        this.mockProcessingMillis = mockProcessingMillis;
     }
 }

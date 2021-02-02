f5748c7bfa3ef563376c9d2baae418b0985464fc
Mike Thompson <mikeylikes23@gmail.com>
2021-02-02 09:50:04 -0700

    Bumped junit version

--- src/main/java/net/mikeyt/logic/QueueOptions.java
+++ src/main/java/net/mikeyt/logic/QueueOptions.java
@@ -1,7 +1,7 @@
 package net.mikeyt.logic;
 
 public class QueueOptions {
-    public static int CONCURRENT_MESSAGE_HANDLERS = 20;
+    public static int CONCURRENT_MESSAGE_HANDLERS = 8;
     public static long GRACEFUL_TERMINATION_SECONDS = 15;
     public static final int QUEUE_POLLING_TIMEOUT_SECONDS = 3;
     public static final int SHUTDOWN_SAFE_TIMEOUT_SECONDS = 5;
